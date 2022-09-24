eafd93684be91f1c54527a88bf07fd5b719b90c6
Tolkien-Dev <felipef685@gmail.com>
2022-09-23 22:07:41 -0300

    I've finished the calculating scripts, and created functions to all buttons.

--- scripts.js
+++ scripts.js
@@ -17,6 +17,40 @@ class Calculator {
     this.clear();
   }
 
+  delete() {
+    this.currentOperand = this.currentOperand.toString().slice(0, -1);
+  }
+
+  calculate() {
+    let result;
+
+    const _previousOperand = parseFloat(this.previousOperand)
+    const _currentOperand = parseFloat(this.currentOperand)
+    
+    if (isNaN(_previousOperand) || isNaN(_currentOperand)) return;
+
+    switch (this.operation) {
+      case '+':
+        result = _previousOperand + _currentOperand;
+        break;
+      case '-':
+        result = _previousOperand - _currentOperand;
+        break;
+      case '*':
+        result = _previousOperand * _currentOperand;
+        break;
+      case '÷':
+        result = _previousOperand / _currentOperand;
+        break;
+      default:
+        return;
+    }
+
+    this.currentOperand = result;
+    this.operation = undefined;
+    this.previousOperand = "";
+  }
+
   chooseOperation(operation) {
     if (this.previousOperand != "") {
       this.calculate();
@@ -24,7 +58,7 @@ class Calculator {
 
     this.operation = operation;
 
-    this.previousOperand = `${this.currentOperand} ${this.operation}`;
+    this.previousOperand = this.currentOperand;
     this.currentOperand = "";
   }
 
@@ -41,7 +75,7 @@ class Calculator {
   }
 
   updateDisplay() {
-    this.previousOperandTextElement.innerText = this.previousOperand;
+    this.previousOperandTextElement.innerText = `${this.previousOperand} ${this.operation || ""}`;
     this.currentOperandTextElement.innerText = this.currentOperand;
   }
 }
@@ -67,5 +101,15 @@ for (const operationButton of operationButtons) {
 
 allClearButton.addEventListener("click", () => {
   calculator.clear();
+  calculator.updateDisplay();
+});
+
+equalsButton.addEventListener("click", () => {
+  calculator.calculate();
+  calculator.updateDisplay();
+});
+
+deleteButton.addEventListener("click", () => {
+  calculator.delete();
   calculator.updateDisplay();
 });
